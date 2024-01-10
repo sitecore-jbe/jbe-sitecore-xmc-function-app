c7cda552b8a71f88b2fd33dd8d85df5c58676ade
sitecore-jbe <jbe@sitecore.net>
2024-01-10 09:24:41 +0100

    Update RequestParameters.cs

--- RequestParameters.cs
+++ RequestParameters.cs
@@ -51,14 +51,46 @@ namespace Sitecore
             log.LogInformation("Request query parameter 'XMCClientSecret': " + instance.XMCClientSecret);
 
             // Only for debugging.
-            if (instance.ContentHubTenantUrl == null) { instance.ContentHubTenantUrl = "https://almu-llbg.sitecoresandbox.cloud"; }
-            if (instance.ContentHubUsername == null) { instance.ContentHubUsername = "JBESitecoreXMC"; }
-            if (instance.ContentHubPassword == null) { instance.ContentHubPassword = "S!t3c0r3"; }
-            if (instance.ContentHubClientId == null) { instance.ContentHubClientId = "JBESitecoreXMC"; }
-            if (instance.ContentHubClientSecret == null) { instance.ContentHubClientSecret = "S!t3c0r3"; }
-            if (instance.XMCTenantUrl == null) { instance.XMCTenantUrl = "https://xmc-sitecoresaacf82-jbexmclouddemo-production.sitecorecloud.io"; }
-            if (instance.XMCClientId == null) { instance.XMCClientId = "xfOoiiLN0Z4UCcr63HaSAcwG52z7INzN"; }
-            if (instance.XMCClientSecret == null) { instance.XMCClientSecret = "mohNocAmpvzgG5I773pwYMfyG3bh8XH7_8ARaDKkyCw_LDtHDz8lItoqPuy1WJQH"; }
+            if (instance.ContentHubTenantUrl == null)
+            {
+                instance.ContentHubTenantUrl = "https://almu-llbg.sitecoresandbox.cloud";
+                log.LogInformation("Request query parameter 'ContentHubTenantUrl' not provided, using default: " + instance.ContentHubTenantUrl);
+            }
+            if (instance.ContentHubUsername == null)
+            {
+                instance.ContentHubUsername = "JBESitecoreXMC";
+                log.LogInformation("Request query parameter 'ContentHubUsername' not provided, using default: " + instance.ContentHubUsername);
+            }
+            if (instance.ContentHubPassword == null)
+            {
+                instance.ContentHubPassword = "S!t3c0r3";
+                log.LogInformation("Request query parameter 'ContentHubPassword' not provided, using default: " + instance.ContentHubPassword);
+            }
+            if (instance.ContentHubClientId == null)
+            {
+                instance.ContentHubClientId = "JBESitecoreXMC";
+                log.LogInformation("Request query parameter 'ContentHubClientId' not provided, using default: " + instance.ContentHubClientId);
+            }
+            if (instance.ContentHubClientSecret == null)
+            {
+                instance.ContentHubClientSecret = "S!t3c0r3";
+                log.LogInformation("Request query parameter 'ContentHubClientSecret' not provided, using default: " + instance.ContentHubClientSecret);
+            }
+            if (instance.XMCTenantUrl == null)
+            {
+                instance.XMCTenantUrl = "https://xmc-sitecoresaacf82-jbexmclouddemo-production.sitecorecloud.io";
+                log.LogInformation("Request query parameter 'XMCTenantUrl' not provided, using default: " + instance.XMCTenantUrl);
+            }
+            if (instance.XMCClientId == null)
+            {
+                instance.XMCClientId = "xfOoiiLN0Z4UCcr63HaSAcwG52z7INzN";
+                log.LogInformation("Request query parameter 'XMCClientId' not provided, using default: " + instance.XMCClientId);
+            }
+            if (instance.XMCClientSecret == null)
+            {
+                instance.XMCClientSecret = "mohNocAmpvzgG5I773pwYMfyG3bh8XH7_8ARaDKkyCw_LDtHDz8lItoqPuy1WJQH";
+                log.LogInformation("Request query parameter 'XMCClientSecret' not provided, using default: " + instance.XMCClientSecret);
+            }
 
             log.LogInformation("'Sitecore.RequestParameters.Initialize' ended.");
             return instance;
